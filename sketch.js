2b54fc96b70114fee06ec30e84ace99adc2cce76
Sabique1234 <66312211+Sabique1234@users.noreply.github.com>
2020-06-20 16:42:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -27,37 +27,47 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	box=createSprite(400,665,200,15);
+	box.shapeColor=color(255,0,0)
+
+	box2=createSprite(300,620,15,100);
+	box2.shapeColor=color(255,0,0)
+
+	box3=createSprite(500,620,15,100);
+	box3.shapeColor=color(255,0,0)
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
-
 	Engine.run(engine);
-  
 }
 
 
-function draw() {
+function draw() 
+{
   rectMode(CENTER);
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
  
+Engine.update(engine);
+
+
 }
 
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    Matter.Body.setStatic(packageBody,false);
-    
+function keyPressed() 
+{
+ if (keyCode === DOWN_ARROW) 
+  {
+    Matter.Body.setStatic(packageBody,false);  
   }
 }
 
